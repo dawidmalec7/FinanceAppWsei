ba12755277fba7e6e3b8694de886227ab8583867
Krystian Smoleń <krsmolen@grupainteger.pl>
2020-01-06 13:10:30 +0100

    add moneyboxes api

---
+++ FinanceAppWsei/ClientApp/src/api/moneyBoxes.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const MoneyBoxes = {
+  add: ({title, target, value}) => {
+    return axios.post(`${hostName}/money-boxes/add-new-money-box`, {
+        "Title": title,
+        "Target": target,
+        "Value": value
+    });
+  },
+  get: () => {
+      return axios.get(`${hostName}/money-boxes/get-money-boxes`);
+  },
+  edit: ({id, title, target}) => {
+      return axios.put(`${hostName}/money-boxes/${id}`, {
+        "Title": title,
+        "Target": target
+      });
+  },
+  delete: (id) => {
+      return axios.delete(`${hostName}/money-boxes/${id}`)
+  },
+
+};
+
+export default Users;
--- FinanceAppWsei/ClientApp/src/api/users.js
+++ FinanceAppWsei/ClientApp/src/api/users.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 const Users = {
   login: (login, password) => {
-    return axios.post('api/login', {
+    return axios.post(`${hostName}/login`, {
         "Login": login,
         "Password": password
     });
   },
   register: ({firstName, lastName, login, password}) => {
-    return axios.post('api/register', {
+    return axios.post(`${hostName}/users/register`, {
       "FirstName": firstName,
       "LastName": lastName,
       "Login": login,
