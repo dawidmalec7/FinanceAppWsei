0527c7fd8b8e8d77bb554f37e0575d4f5128548c
Krystian Smoleń <krsmolen@grupainteger.pl>
2020-01-28 21:18:11 +0100

    make tests more dry

---
+++ Tests/config/TestConfig.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    public class TestConfig : Variables
+    {
+        [SetUp]
+        public void Setup()
+        {
+            ConfigChromedriver();
+        }
+
+        [TearDown]
+        public void CloseDriver()
+        {
+            driver.Close();
+        }
+    }
+}
---
+++ Tests/config/Variables.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Tests
+{
+    public class Variables
+    {
+        public string ChromedriverPath = "/Users/krystiansmolen/Downloads";
+        public string Host = "http:localhost:3000";
+        public ChromeDriver driver;
+
+        public void ConfigChromedriver()
+        {
+            driver = new ChromeDriver(ChromedriverPath);
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(Host);
+        }
+    }
+}
