67e7d3cf4129a9463402621e43e0f4e3eef5e32f
Krystian Smoleń <krsmolen@grupainteger.pl>
2020-01-30 19:50:58 +0100

    add context tests

---
+++ Tests/Context/CategoriesTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+using FinanceAppWsei.Models;
+using Tests.config;
+
+namespace Tests
+{
+    class CategoiesTests : DatabaseVariables
+    {
+        [Test]
+        public void ShouldCreateCategoryWithCorrectValues()
+        {
+            SetupDB();
+            Guid userId = new Guid();
+            databaseContext.Users.Add(new User
+            {
+                Id = userId,
+                FirstName = "name",
+                LastName = "nameeee",
+                Login = "test232",
+                Password = "42342kjjjHKj",
+                CreatedOn = new DateTime(),
+            });
+            databaseContext.SaveChanges();
+
+
+            Category category = new Category
+            {
+                Id = new Guid(),
+                Title = "Title",
+                CreatedBy = userId,
+                CreatedOn = DateTime.Now
+            };
+
+            databaseContext.Categories.Add(category);
+            var saveCategory = databaseContext.SaveChanges();
+            Assert.AreEqual(saveCategory, 1);
+        }
+    }
+}
